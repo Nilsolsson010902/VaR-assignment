3486d183ffbb7d441c55a263ee645cd517772f95
Nilsolsson010902 <nilsjohn6@gmail.com>
2026-05-18 17:35:42 +0200

    var simulation functions added

--- assigment.py
+++ assigment.py
@@ -16,12 +16,23 @@ def tasl1d(bond: Bond):
 def task2(var: VaR, Z: float):
     print( var.delta_normal(Z))
 
+def task3(var: VaR):
+    print("\n--- Monte Carlo Simulation VaR ---")
+
+    delta_var = var.simulated_delta_var(confidence=0.95,n=1000)
+
+    delta_gamma_var = var.simulated_delta_gamma_var(confidence=0.95, n=1000)
+
+    print("Simulated delta VaR:", delta_var)
+    print("Simulated delta-gamma VaR:", delta_gamma_var)
+
 
 if __name__ == "__main__":
     bond = Bond(maturity=6.0, ytm =0.03,nominal_amount=12000, coupon_rate=0.05 ) 
-    var = VaR(bond, sigma = 0.04, mu = 0)
+    var = VaR(bond, sigma = 0.02, mu = 0)
     task1a(bond)
     task1b(bond)
     task1c(bond)
     tasl1d(bond)
     task2(var, 1.645)
+    task3(var)
--- var.py
+++ var.py
@@ -20,11 +20,15 @@ class VaR:
     def simulate_y_tilde(self, n=1000):
         return np.random.normal(self.mu, self.sigma, n)
     
+    
     def simulated_delta_var(self, confidence=0.95, n=1000):
-        y_tilde = self.simulate_y_tilde(n, seed)
-
+        y_tilde = self.simulate_y_tilde(n)
         x = -self.delta() * y_tilde
-
         threshold = np.percentile(x, (1 - confidence) * 100)
-
+        return abs(threshold) * self.bond.price()
+    
+    def simulated_delta_gamma_var(self, confidence=0.95, n=1000):
+        y_tilde = self.simulate_y_tilde(n)
+        x = -self.delta() * y_tilde + 0.5 * self.gamma() * y_tilde**2
+        threshold = np.percentile(x, (1 - confidence) * 100)
         return abs(threshold) * self.bond.price()
